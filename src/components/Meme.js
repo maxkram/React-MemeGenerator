4345ba55def6b5afc1af909a3fdda64d8c1bb30a
maxkram <max.kramer@live.com>
2022-06-17 11:29:40 -0400

    feat: add handleChange to get input from form

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -19,6 +19,12 @@ export default function Meme() {
             randomImage: url,
         }));
     }
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setMeme((prevMeme) => ({ ...prevMeme, [name]: value }));
+    }
+
     return (
         <main>
             <div className='form'>
@@ -26,17 +32,31 @@ export default function Meme() {
                     type='text'
                     placeholder='Top text'
                     className='form--input'
+                    name='topText'
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input
                     type='text'
                     placeholder='Bottom text'
                     className='form--input'
+                    name='bottomText'
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button className='form--button' onClick={getMemeImage}>
                     Get a new image
                 </button>
             </div>
-            <img src={meme.randomImage} alt='meme' className='meme--image' />
+            <div className='meme'>
+                <img
+                    src={meme.randomImage}
+                    alt='meme'
+                    className='meme--image'
+                />
+                <h2 className='meme--text top'>{meme.topText}</h2>
+                <h2 className='meme--text bottom'>{meme.bottomText}</h2>
+            </div>
         </main>
     );
 }
